555345fc98616d74fd6ce280aabd60757f2a020b
Kariuki11 <irungukenneth433@gmail.com>
2025-03-10 01:16:20 +0300

    Add search functionality and navigation links to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import {FaSearch} from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <header className='font-bold text-sm sm:text-xl flex flex-wrap'>
-        <span className='text-slate-400'>Prime</span>
-        <span className='text-slate-900'>Horizon</span>
+    <header className='bg-blue-400 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
+        <Link to='/'>
+          <h1 className='font-bold italic text-sm sm:text-xl flex flex-wrap skew-x-6'>
+            <span className='text-slate-900'>Prime</span>
+            <span className='text-red-500'>Horizon</span>
+          </h1>
+        </Link>
+        <form className='bg-slate-200 p-3 rounded-lg flex items-center'>
+          <input type='text' placeholder='Find...'
+            className='bg-transparent focus:outline-none w-24 sm:w-64'/>
+            <FaSearch className='text-slate-400'/>
+        </form>
+        <ul className='flex gap-5'>
+          <Link to='/'>
+            <li className='hidden sm:inline text-white hover:underline'>
+              Home
+            </li>
+          </Link>
+          <Link to ='/about'>
+            <li className='hidden sm:inline text-white hover:underline'>
+              About
+            </li>
+          </Link>
+          <Link to = '/Sign-in'>
+            <li className='text-white hover:underline'>
+              Sign in
+              </li>
+          </Link>
+        </ul>
+      </div>
     </header>
   )
 }
 
 export default Header
+
+
