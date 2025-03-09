dfe1d5dba153258bbc062c116b4b3236c7c9d316
Kariuki11 <irungukenneth433@gmail.com>
2025-03-09 14:44:43 +0300

    New updates

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+
+
+
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // require('@tailwindcss/line-clamp'),
+    // ...
+  ],
+};
+
+
+
+
+
+
+
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
