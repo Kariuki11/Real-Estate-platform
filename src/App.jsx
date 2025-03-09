dc6b91bf3b521f93a74be4c708627b6c25cbff88
Kariuki11 <irungukenneth433@gmail.com>
2025-03-09 16:33:24 +0300

    Add routing and new pages for Home, About, SignUp, Signin, and Profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,74 @@
-import React from 'react'
-
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignUp from "./pages/SignUp"
+import Signin from "./pages/Signin"
+import Profile from "./pages/Profile"
 function App() {
   return (
-    <div className='text-red-500'>App</div>
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/sign-in" element={<Signin/>} />
+          <Route path="/sign-up" element={<SignUp/>} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile/>} />
+        </Routes>
+    </BrowserRouter>
   )
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { BrowserRouter, Routes, Route } from "react-router-dom"
+// import Home from "./pages/Home"
+// import About from "./pages/About"
+// import SignUp from "./pages/SignUp"
+// import Signin from "./pages/Signin"
+// import Profile from "./pages/Profile"
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//         <Routes>
+//           <Route path="/" element={<Home/>} />
+//            <Route path="/sign-in" element={<Signin/>} />
+//            <Route path="/sign-up" element={<SignUp/>} />
+//            <Route path="/about" element={<About />} />
+//            <Route path="/profile" element={<Profile/>} />
+//          </Routes>
+//      </BrowserRouter>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+
+
+
+
+
