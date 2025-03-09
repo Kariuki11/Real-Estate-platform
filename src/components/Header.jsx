5948ac0206eafae67c07b98c9af11b9c3163ad3c
Kariuki11 <irungukenneth433@gmail.com>
2025-03-10 00:30:50 +0300

    Add Header component and integrate it into the main App

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className='font-bold text-sm sm:text-xl flex flex-wrap'>
+        <span className='text-slate-400'>Prime</span>
+        <span className='text-slate-900'>Horizon</span>
+    </header>
+  )
+}
+
+export default Header
