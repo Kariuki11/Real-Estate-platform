9e9e8040a2ade1e5cadbd813944bf41efbe5c31c
Kariuki11 <irungukenneth433@gmail.com>
2025-03-11 23:43:02 +0300

    Refactor SignUp component; implement form submission handling and improve input structure

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -10,17 +10,37 @@ function SignUp() {
       {
         ...formData,
         [e.target.id]: e.target.value
-      }
-    )
-  }
+      });
+  };
+  console.log(formData)
 
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>SIGN U;;;;;;;;;;;;;;;;;;;;;;;;P</h1>
-      <form className='flex flex-col items-center gap-4'>
-        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username' onChange={handleChange}/>
-        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email' onChange={handleChange}/>
-        <input type="password" placeholder='password' className='border p-3 roundd-lg' id='password' onChange={handleChange}/>
+      <form onSubmit={handleSubmit} className='flex flex-col items-center gap-4'>
+        <input
+          type="text"
+          placeholder='username'
+          className='border p-3 rounded-lg'
+          id='username'
+          onChange={handleChange}
+        />
+
+        <input
+          type="email"
+          placeholder='email'
+          className='border p-3 rounded-lg'
+          id='email'
+          onChange={handleChange}
+        />
+
+        <input type="password"
+          placeholder='password'
+          className='border p-3 roundd-lg'
+          id='password'
+          onChange={handleChange}
+        />
+        
         <button className='bg-slate-700 text-white p-4 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
       </form>
       <div className="flex gap-2 mt-5">
