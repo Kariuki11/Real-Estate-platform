b178edaf4daa3d3ea17a3c4c175977154fee1edd
Kariuki11 <irungukenneth433@gmail.com>
2025-03-11 23:26:22 +0300

    Update SignUp page layout and styling; add email and password fields, and navigation to Sign in

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
-import { Input } from "@chakra-ui/react";
+import { Link } from 'react-router-dom'
+
 
 
 function SignUp() {
   return (
-    <div>
+    <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>SIGN U;;;;;;;;;;;;;;;;;;;;;;;;P</h1>
-      <form>
-        <Input type="text" placeholder="username"></Input>
-        <input type="text" placeholder="Username" className="border p-2 w-full rounded" />
+      <form className='flex flex-col items-center gap-4'>
+        <input type="text" placeholder='username' className='border p-3 rounded-lg' id='username'/>
+        <input type="email" placeholder='email' className='border p-3 rounded-lg' id='email'/>
+        <input type="password" placeholder='password' className='border p-3 roundd-lg' id='password'/>
+        <button className='bg-slate-700 text-white p-4 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Sign up</button>
       </form>
+      <div className="flex gap-2 mt-5">
+        <p>Account Exist? Login</p>
+        <Link to={"/sign-in"}>
+          <span className='text-blue-800'>Sign in</span>
+        </Link>
+      </div>
     </div>
   )
 }
