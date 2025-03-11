c1ad73ba56bafe540daaa2e7df74011703ed1d50
Kariuki11 <irungukenneth433@gmail.com>
2025-03-11 15:51:10 +0300

    Rename Signup component to SignUp for consistency and fix header import; update SignUp page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import About from "./pages/About"
-import SignUp from "./pages/Signup"
+import SignUp from "./pages/SignUp"
+// import SignUp from "./pages/Signup";
 import Signin from "./pages/Signin"
 import Profile from "./pages/Profile"
 import Header from "./components/Header"
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Input } from "@chakra-ui/react";
+
+
+function SignUp() {
+  return (
+    <div>
+      <h1 className='text-3xl text-center font-semibold my-7'>SIGN U;;;;;;;;;;;;;;;;;;;;;;;;P</h1>
+      <form>
+        <Input type="text" placeholder="username"></Input>
+        <input type="text" placeholder="Username" className="border p-2 w-full rounded" />
+      </form>
+    </div>
+  )
+}
+
+export default SignUp
